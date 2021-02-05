6441c2165d816bef00269e35ef48390bc92f1d7a
Sami Triki <contact@triki.io>
2021-02-05 12:39:37 +0000

    Data: Remove redundant date casting, import more data points from CSV

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,4 @@
-import { formatDatesForChart } from './utils'
+import { transactionsPerPeriod } from './utils'
 import * as V from 'victory'
 
 function Chart({ transactions}) {
@@ -7,29 +7,28 @@ function Chart({ transactions}) {
     return <p>Waiting for data to draw chart</p>
   }
 
-  const transactionsByDay = formatDatesForChart(transactions)
+  const transactionsByDay = transactionsPerPeriod(transactions, 'day')
 
-  // inneficient change later
-  let formattedata = Object.keys(transactionsByDay)
-    .reduce((dates, current, idx) => {
-      if (transactionsByDay[current].change) { // only show change in portfolio value, update with show nil transactions
+  const dataForChart = Object.keys(transactionsByDay)
+    .reduce((dates, current) => {
+      if (transactionsByDay[current].value) { // only show change in portfolio value, update with show nil transactions
         return [...dates, {
           cumul: transactionsByDay[current].total,
-          change: transactionsByDay[current].change,
-          date: new Date(current).getTime()
+          value: transactionsByDay[current].value,
+          date: transactionsByDay[current].date,
+          timestamp: new Date(current).getTime()
         }]
       }
       return dates
     }, [])
 
-
   return (
     <V.VictoryChart padding={{top: 10, left: 30, bottom: 30}} theme={V.VictoryTheme.material}>
       <V.VictoryAxis
         padding={0}
-        tickValues={formattedata.filter(d => d.change >= 10).map(d => new Date(d.date))}
-        tickFormat={formattedata.filter(d => d.change >= 10).map(day => {
-          let d = new Date(day.date);
+        tickValues={dataForChart.filter(d => d.value >= 10).map(d => d.date)}
+        tickFormat={dataForChart.filter(d => d.value >= 10).map(day => {
+          let d = day.date;
           let formattedDate = `${d.getMonth()+1}/${d.getFullYear().toString().substring(2, 4)}`;
           return formattedDate
         })}
@@ -46,7 +45,7 @@ function Chart({ transactions}) {
         tickLabels: { fontSize: 5}
       }}/>
       <V.VictoryLine
-        data={formattedata}
+        data={dataForChart}
         style={{
           data: {
             stroke: "#009fdf",
@@ -57,7 +56,7 @@ function Chart({ transactions}) {
             fontSize: 10
           }
         }}
-        x="date"
+        x="timestamp"
         y="cumul"
       />
     </V.VictoryChart>
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,15 @@
 
 // { Date: '20/10/2020', Time: '10:50'} => Date Object
-const stringToDate = (transactionFromCSV) => {
-  let [day, month, year] = transactionFromCSV.Date.split('-')
-  let [hour, second] = transactionFromCSV.Time?.split(':')
+const csvTransactionToDate = (csvTransaction) => {
+  let [day, month, year] = csvTransaction.Date.split('-')
+  let [hour, second] = csvTransaction.Time?.split(':')
   // Month is 0 indexed
   return new Date(year, month - 1, day, hour, second);
 }
 
 // Date Object => '2020-01-29'
-const dateToString = (date) => {
-  let [day, month, year] = date.toLocaleDateString("en-GB", { // you can skip the first argument
+const csvDateToString = (date) => {
+  let [day, month, year] = date.toLocaleDateString("en-GB", {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
@@ -18,21 +18,27 @@ const dateToString = (date) => {
   return `${year}-${month}-${day}`
 }
 
-function formatTransactions(CSVResults) {
+function transactionsFromCSV(CSVResults) {
   return CSVResults
     .filter(t => t)
     .filter(t => t.Date && t.Time)
     .map(transaction => {
-      const { Value, Product, Description, Currency} = transaction
-      let date = stringToDate(transaction)
-      let dateString = dateToString(date)
+      const { Value, Product, Description, Currency, ISIN, FX} = transaction
+      let date = csvTransactionToDate(transaction)
+      let dateString = csvDateToString(date)
+      let timestamp = date.getTime()
 
       return {
         date,
         dateString,
+        timestamp,
         value: Value ? parseFloat(Value) : 0,
-        description: `${Product} ${Description}`,
-        currency: Currency
+        product: Product,
+        description: Description,
+        currency: Currency,
+        isin: ISIN,
+        fx: FX,
+        orderId: transaction['Order Id']
       }
     })
     .sort((a, b) => a.date.getTime() - b.date.getTime())
@@ -40,27 +46,28 @@ function formatTransactions(CSVResults) {
 
 
 // [{date, value, name, currency }, {...}] to =>
-// {"2020-01-01": { change: 100, total: 200, transactions: 3}, "2020-01-20":{...}}
-const formatDatesForChart = (transactions, period = 'day') => transactions.reduce((dates, current) => {
+// {"2020-01-01": { value: 100, total: 200, transactions: 3}, "2020-01-20":{...}}
+const transactionsPerPeriod = (transactions, period = 'day') => transactions.reduce((dates, current) => {
   const [year, month, day] = current.dateString.split('-')
   let dateString = period === 'day' ? `${year}-${month}-${day}` : period === 'month' ? `${year}-${month}` : year
   let previousPeriodKey = Object.keys(dates)[Object.keys(dates).length-1];
   let prevTotal = dates[previousPeriodKey]?.total || 0
 
   if (dates[dateString]) {
-      dates[dateString].change += current.value
+      dates[dateString].value += current.value
       dates[dateString].total += current.value
       dates[dateString].transactions++
   } else {
       dates[dateString] = {
-        change: current.value,
+        ...current,
+        value: current.value,
         total: current.value + prevTotal,
         transactions: current.value ? 1 : 0,
-        prevPeriod: previousPeriodKey
+        prevPeriod: previousPeriodKey,
       };
   }
 
   return dates
 }, {})
 
-export { formatTransactions, formatDatesForChart }
+export { transactionsFromCSV, transactionsPerPeriod }
